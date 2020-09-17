be8ecd01180f3a671dbf3984ee3c487e19393ab8
Marta <m.matysek95@wp.pl>
2020-09-17 20:39:31 +0200

    'changing winning score' logic removed
    'changing winning score' logic removed

--- game.js
+++ game.js
@@ -26,9 +26,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
         scores[activePlayer] += roundScore;
         document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
 
-        winningScore = document.querySelector('.winning-score').value;
-
-        if ((winningScore && scores[activePlayer] > winningScore) || scores[activePlayer] > 100) {
+        if (scores[activePlayer] > 100) {
             document.querySelector('#player-' + activePlayer).textContent = "Winner!";
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
             document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
@@ -45,7 +43,6 @@ document.querySelector('.btn-new').addEventListener('click', init);
 
 function changePlayer() {
     roundScore = 0;
-    previousScore = 0;
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
     document.getElementById('current-score-0').textContent = 0;
     document.getElementById('current-score-1').textContent = 0;
